f87bbedf2bb0b8705d2a118e9943a927362e70f2
AndreyTerechenko <Dronka97@yandex.ru>
2024-03-12 00:23:24 +0300

    error correction

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,16 @@ public class Main {
         System.out.println("Задача 1");
         int age = 17;
         if (age >= 18) {
-            System.out.println("Совернешолетие наступило");
-        } else System.out.println("Совершенолетие еще не наступило, надо немного подождать.");
+            System.out.println("Если возраст человека равен " + age + ", то он  достиг совершеннолетия.");
+        } else
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать");
         //надеюсь задание понял правильно.
         System.out.println("\nЗадача 2");
 
         int temperature = 10;
         if (temperature >= 5) {
-            System.out.println("Сегодня тепло, можно идти без шапки. Наулице " + temperature + " " +
-                    "градусов");
-        } else System.out.println("На улице холодно, нужно надеть шапку. На улице " + temperature + " " +
-                "градусов");
+            System.out.println("Сегодня тепло, можно идти без шапки. Наулице " + temperature + "градусов");
+        } else System.out.println("На улице холодно, нужно надеть шапку. На улице " + temperature + "градусов");
 
         System.out.println("\nЗадача 3");
         int speed = 59;
@@ -29,14 +28,11 @@ public class Main {
         if (humanAge <= 1) {
             System.out.println("Если возраст человка равен " + humanAge + " то пусть сидит дома :)");
         } else if (humanAge >= 2 && humanAge <= 6) {
-            System.out.println("Если возраст человека равен " + humanAge + ", то ему нужно ходить в" +
-                    " детский сад.");
+            System.out.println("Если возраст человека равен " + humanAge + ", то ему нужно ходить в детский сад.");
         } else if (humanAge >= 7 && humanAge <= 17) {
-            System.out.println("Если возраст человека равен " + humanAge + "," +
-                    " то ему нужно ходить в школу");
+            System.out.println("Если возраст человека равен " + humanAge + ",то ему нужно ходить в школу");
         } else if (humanAge >= 18 && humanAge <= 24) {
-            System.out.println("Если возраст человека равен " + humanAge + ", то ему нужно " +
-                    "ходить в универститет");
+            System.out.println("Если возраст человека равен " + humanAge + ", то ему нужно ходить в универститет");
         } else if (humanAge > 24) {
             System.out.println("А если человеку больше 24, то ему пора ходить на работу.");
         }
@@ -45,14 +41,11 @@ public class Main {
         // Как правило, на аттракционах действуют ограничения для детей по возрасту:
         int childAge = 17;
         if (childAge >= 5 && childAge <= 14) {
-            System.out.println("Если возраст ребенка равен " + childAge + "," +
-                    " то ему можно кататся на атракционе только в сопровождении взрослого");
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему можно кататся на атракционе только в сопровождении взрослого");
         } else if (childAge < 5) {
-            System.out.println("Если возраст ребенка равен " + childAge + "," +
-                    " то ему нельзя кататся на атраеционе");
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему нельзя кататся на атраеционе");
         } else if (childAge > 14) {
-            System.out.println("Если возраст ребенка равен " + childAge + ", " +
-                    "то ему можно кататся без сопровождения взрослого");
+            System.out.println("Если возраст ребенка равен " + childAge + ", то ему можно кататся без сопровождения взрослого");
         }
         System.out.println("\nЗадача 6");
 
@@ -74,11 +67,11 @@ public class Main {
         int three = 3787878;
         if (three > two && three > one) {
             System.out.println("Число " + three + ", больше чем число " + two + " и " + one);
+        } else if (one > two && one > three) {
+            System.out.println("Число " + one + " больше чем число " + two + " и число " + three);
+        } else if (two > one && two > three) {
+            System.out.println("Число " + two + " больше чем число " + one + " и число " + three);
         }
-        // Задание показалось слишком легким. возможно надо было сделать как то иначе, но IDEA
-        // требует инициализровать переменные, пришлось подписать цифры.
-        // Прошу разяснения, если сделал не так.
-        else System.out.println("Не верно.");
 
     }
 }
